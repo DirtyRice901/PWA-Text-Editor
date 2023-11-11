635d88a95732b50da70d36af4794fe6f6bb02be7
Kevin Rice <reatriniti@gmail.com>
2023-11-10 22:00:51 -0500

    added worbox plugins for sw and manifest file  and npm install --save-dev webpack-pwa-manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,7 +7,11 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
 // TODO: Add CSS loaders and babel to webpack.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
 
 module.exports = () => {
   return {
@@ -21,6 +25,32 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+          {
+            src: path.resolve('src/assets/large-icon.png'),
+            size: '1024x1024' // you can also use the specifications pattern
+          },
+          {
+            src: path.resolve('src/assets/maskable-icon.png'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'sw.js',
@@ -29,6 +59,20 @@ module.exports = () => {
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          },
+        }
         
       ],
     },
